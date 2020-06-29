61e10bfda871d1b4e36512984270f0c45a93c483
Mar Mustafa <amustaf@email.arizona.edu>
2020-06-29 20:31:05 +0000

    Automatically backed up by Learn

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -36,8 +36,7 @@ second_number = get_variable_from_file('./calculator.rb', "second_number")
   end
 
   it "contains a local variable called quotient that is assigned to the result of dividing first_number by second_number" do
-    first_number = get_variable_from_file('./calculator.rb', "first_number")
-    second_number = get_variable_from_file('./calculator.rb', "second_number")
+
     quotient = get_variable_from_file('./calculator.rb', "quotient")
 
     expect(quotient).to eq(first_number/second_number)
